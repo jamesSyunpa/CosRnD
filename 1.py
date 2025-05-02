4a8714f9b7f88ce77b78bfe3f5ffa61da328bf19
jamesSyunpa <syunpa1944@gmail.com>
2025-05-02 10:30:00 +0900

    CosRQD Development Stage 01: 처방개발기
    - Original Development Date: 2025-05-02 10:30:00
    - IP & Copyright Prior Art Creation Record
    - Author: jamesSyunpa

---
+++ 1.py
@@ -0,0 +1,5 @@
+pyinstaller --noconsole --onefile --add-data "data_ing.xls;." --add-data "hlb값.xlsx;." 개발기.py
+
+
+pyinstaller --noconsole --onefile --add-data "data_ing.xls;." --add-data "hlb값.xlsx;." HLB_제형_설계기_통합버전.py
+
